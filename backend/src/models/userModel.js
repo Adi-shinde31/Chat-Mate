47905115e6fcb1e5336d4717bb64f2f68b3c7d99
Aditya <shindeaditya0258@gmail.com>
2026-01-25 12:32:43 -0600

    add: User model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        "email": {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        "fullName": {
+            type: String,
+            required: true,
+        },
+        "password": {
+            type: String,
+            required: true,
+            minlength: 6,
+            select: false,
+        },
+        "profilePicture": {
+            type: String,
+            default: '',
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
